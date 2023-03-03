0d23128420603bcdd781391cda572fa799c429af
asmaabahaki1 <122092501+asmaabahaki1@users.noreply.github.com>
2023-03-03 11:26:32 +0100

    Delete accueil.js

--- accueil/accueil.js
+++
@@ -1,7 +0,0 @@
-// step1: const favorites = JSON.parse(localStorage.getItem('favorites'))
-
-// step 2: check value of favorites if is it null
-
-// step 3 : if its null : do nothing
-
-// step 4 : if its not null (it will be array of ids): 
